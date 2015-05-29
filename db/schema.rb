d8facafd0b6ed1e2bb56471098d57dfdc7bcd512
Gareth Connop <g.connop@btinternet.com>
2015-05-29 12:14:46 +0100

    Added forum model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528191216) do
+ActiveRecord::Schema.define(version: 20150529110659) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "forums", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
