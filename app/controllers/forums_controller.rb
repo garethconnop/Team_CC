43fe5f066ac97c6a5de715f21d5308c7d551cdd2
Gareth Connop <g.connop@btinternet.com>
2015-05-29 16:24:09 +0100

    Added authentication to forums

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,5 +1,6 @@
 class ForumsController < ApplicationController
   before_action :find_forum, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@forums = Forum.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
