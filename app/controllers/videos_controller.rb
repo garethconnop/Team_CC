f37729920e20341c508808f8401c05bcc383f041
Gareth Connop <g.connop@btinternet.com>
2015-05-30 01:30:41 +0100

    Added authentication to the videos controller and added comment count to the forum index page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,7 @@
 class VideosController < ApplicationController
 	before_action :find_video, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, except: [:index, :show]
+
 	def index
 		if params[:video_category].blank?
 			@videos = Video.all.order("created_at desc").paginate(page: params[:page], per_page: 12)
