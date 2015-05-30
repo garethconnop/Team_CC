bc25a8dfa87938a2841bab7e5b74d59431d8f8cc
Gareth Connop <g.connop@btinternet.com>
2015-05-30 11:42:46 +0100

    Added avatars to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,34 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :forums
   has_many :forum_comments
+
+  #Login with username or email
+  #attr_accessor :login
+
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   else
+  #     where(conditions.to_h).first
+  #   end
+  # end
+
+ #  def self.find_for_database_authentication(warden_conditions)
+	#   conditions = warden_conditions.dup
+	#   if login = conditions.delete(:login)
+	#     where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+	#   else
+	#     where(conditions).first
+	#   end
+	# end
+
+ #  validates :username, presence: true, length: {maximum: 24}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "can only contain letters and numbers, and be a maximum of 24 characters." }
+
+ #Avatar
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }#, default_url: ":style/user.jpg"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529162805) do
+ActiveRecord::Schema.define(version: 20150530102347) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -64,6 +64,11 @@ ActiveRecord::Schema.define(version: 20150529162805) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
