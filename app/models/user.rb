49c3c5ce63738a49455fdfb50485ae930848f25d
Gareth Connop <g.connop@btinternet.com>
2015-06-02 15:43:35 +0100

    Added ability to enable user to login with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,27 +8,18 @@ class User < ActiveRecord::Base
   has_many :forum_comments
 
   #Login with username or email
-  #attr_accessor :login
+  attr_accessor :login
 
-  # def self.find_for_database_authentication(warden_conditions)
-  #   conditions = warden_conditions.dup
-  #   if login = conditions.delete(:login)
-  #     where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-  #   else
-  #     where(conditions.to_h).first
-  #   end
-  # end
+  def self.find_for_database_authentication(warden_conditions)
+	  conditions = warden_conditions.dup
+	  if login = conditions.delete(:login)
+	    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+	  else
+	    where(conditions).first
+	  end
+	end
 
- #  def self.find_for_database_authentication(warden_conditions)
-	#   conditions = warden_conditions.dup
-	#   if login = conditions.delete(:login)
-	#     where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-	#   else
-	#     where(conditions).first
-	#   end
-	# end
-
- #  validates :username, presence: true, length: {maximum: 24}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "can only contain letters and numbers, and be a maximum of 24 characters." }
+  validates :username, presence: true, length: {maximum: 24}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "can only contain letters and numbers, and be a maximum of 24 characters." }
 
  #Avatar
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }#, default_url: ":style/user.jpg"
--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150602002613) do
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "forum_categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -85,6 +79,7 @@ ActiveRecord::Schema.define(version: 20150602002613) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "video_categories", force: :cascade do |t|
     t.string   "name"
@@ -104,7 +99,6 @@ ActiveRecord::Schema.define(version: 20150602002613) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "slug"
-    t.integer  "category_id"
     t.string   "video_category_id"
   end
 
